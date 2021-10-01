0890b6f03e960c379bcb19b2bf9c41d42e51cd51
apploid <apploidyakutsk@gmail.com>
2021-10-01 15:13:50 +0300

    add current time on map

--- src/components/ServerTable.js
+++ src/components/ServerTable.js
@@ -1,5 +1,18 @@
 import {Table} from "antd";
 
+function format_time(s) {
+    const dtFormat = new Intl.DateTimeFormat('en-GB', {
+        timeStyle: 'medium',
+        timeZone: 'UTC'
+    });
+
+    return dtFormat.format(new Date(s * 1e3));
+}
+
+function time_diff_from_now(date) {
+    return new Date().getTime() / 1000 - date;
+}
+
 const ServerTable = ({serversInfo}) => {
 
     const columns = [
@@ -12,6 +25,11 @@ const ServerTable = ({serversInfo}) => {
             title: "Текущая карта",
             key: "current_map",
             render: (text, record) => <div>{record.current_map.human_name}</div>
+        },
+        {
+            title: "Время",
+            key: "current_map",
+            render: (text, record) => <div>{format_time(time_diff_from_now(record.current_map.start))}</div>
         }, {
             title: "Количество игроков",
             dataIndex: "nb_players",
